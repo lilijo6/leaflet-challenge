e4188520d4d999ba232eaf9847ce735df685b39d
lilijo6 <64238731+lilijo6@users.noreply.github.com>
2020-09-12 00:19:53 -0400

    final commit - fixed legend and markers

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,8 +1,8 @@
 //Activity 1 Day 2
 // Creating map object
 var myMap = L.map("map", {
-    center: [0, 0],
-    zoom: 5
+    center: [14.2350, -14.9253],
+    zoom: 3
 });
 
 // Adding tile layer to the map
@@ -18,60 +18,61 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 // Load in geojson data
 var geoData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-// Function that will determine the color of the marker based on the magnitude
-function chooseColor(mag) {
-    if (mag < 3.9) {
-        return "green"
-    } else if (mag < 4.9) {
-        return "#ADFF2F"
-    } else if (mag < 5.9) {
-        return "yellow"
-    } else if (mag < 6.9) {
-        return "orange"
-    } else if (mag < 7.9) {
-        return "red"
-    } else {
-        return "#8B0000"
-    }
-};
-
-//Activity 9 Day 1
-// Function to determine marker size based on magnitude
-function markerSize(mag) {
-    if (mag > 0) {
-        return mag * 100000
-    } else {
-        return 1
-    }
-};
-
+//creating earthquake data layer group
+var earthquakes = new L.LayerGroup();
 
 //Grab data with d3
 
 // Grabbing our GeoJSON data..
 d3.json(geoData, function(data) {
+
+    // Function that will determine the color of the marker based on the magnitude classification for earthquakes
+    function chooseColor(mag) {
+        if (mag < 3.9) {
+            return "green"
+        } else if (mag < 4.9) {
+            return "#ADFF2F"
+        } else if (mag < 5.9) {
+            return "yellow"
+        } else if (mag < 6.9) {
+            return "orange"
+        } else if (mag < 7.9) {
+            return "red"
+        } else {
+            return "#8B0000"
+        }
+    };
+
+    //Activity 9 Day 1
+    // Function to determine marker size based on magnitude
+    function markerSize(mag) {
+        if (mag > 0) {
+            return mag * 5
+        } else {
+            return 1
+        }
+    };
+
     // Creating a geoJSON layer with the retrieved data
     L.geoJson(data, {
-        style: function(features) {
+        pointToLayer: function(feature, latlong) {
+            return L.circleMarker(latlong);
+        },
+        style: function(feature) {
             return {
                 color: "white",
-                fillColor: chooseColor(features.properties.mag),
+                fillColor: chooseColor(feature.properties.mag),
                 fillOpacity: 1,
                 weight: 1.5,
-                radius: markerSize(features.properties.mag)
+                radius: markerSize(feature.properties.mag)
             };
         },
         // Binding a pop-up to each layer
-        onEachFeature: function(features, layer) {
-            layer.bindPopup("Earthquake Magnitude: " + features.properties.mag + "<br>Location: <br>" +
-                +features.properties.place);
+        onEachFeature: function(feature, layer) {
+            layer.bindPopup("Earthquake Magnitude: " + feature.properties.mag + "<br>Location: <br>" +
+                +feature.properties.place);
         },
-        pointToLayer: function(features, latlng) {
-            return
-            // Create a circle and pass in some initial options
-            L.circleMarker(latlng)
 
-        }
     }).addTo(myMap);
 
     // // // Define arrays to hold created earthquake markers
@@ -80,26 +81,18 @@ d3.json(geoData, function(data) {
 
     // Set up the legend
     var legend = L.control({ position: "bottomright" });
-    legend.onAdd = function() {
+    legend.onAdd = function(myMap) {
         var div = L.DomUtil.create("div", "info legend");
-        var limits = [0, 3.9, 4.9, 5.9, 6.9, 7.9, 10];
+        var grades = [0, 3.9, 4.9, 5.9, 6.9, 7.9]
         var colors = ["green", "#ADFF2F", "yellow", "orange", "red", "purple"];
-        var labels = [];
+        //var labels = [];
 
-        //   // Add min & max
-        //   var legendInfo = "<h1>Median Income</h1>" +
-        //     "<div class=\"labels\">" +
-        //       "<div class=\"min\">" + limits[0] + "</div>" +
-        //       "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
-        //     "</div>";
-
-        //   div.innerHTML = legendInfo;
-
-        limits.forEach(function(limit, index) {
-            labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
-        });
-
-        div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+        // Looping through
+        for (var i = 0; i < grades.length; i++) {
+            div.innerHTML +=
+                "<i style='background: " + colors[i] + "'></i> " +
+                grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
+        }
         return div;
     };
 
